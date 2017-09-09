eac8b25f63e952ab108438bd5596581cc4e744e8
Orie Steele <orie@ohs.io>
2017-09-09 15:42:26 -0500

    update example to token

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Tangle ICO
 
-A single page splash screen for showing ico information in an explorable manner.
+A single page splash screen for showing ico or token sale information in an explorable manner.
--- src/scripts.js
+++ src/scripts.js
@@ -2,21 +2,13 @@ function setUpTangle() {
     var element = document.getElementById("example");
     var tangle = new Tangle(element, {
         initialize: function () {
-            this.yearly_net_revenue = 100;
-            this.yearly_dividend_revenue_percent = 10;
-
-            this.token_total = 1000;
-            this.token_holdings = 10;
-            this.total_shareholder_payout = 0;
-            this.share_holder_percent = 0;
-            this.share_holder_earnings = 0;
-            // this.cookies = 4;
+            this.cost_cloud_in_usd = .5;
+            this.cost_usd_in_eth = 1/300;
         },
         update: function () {
-            this.total_shareholder_payout = this.yearly_net_revenue * this.yearly_dividend_revenue_percent / 100;
-            this.share_holder_percent = this.token_holdings / this.token_total * 100;
-            this.share_holder_earnings = this.total_shareholder_payout * this.share_holder_percent / 100;
-            // this.calories = this.cookies * this.caloriesPerCookie;
+            this.cost_eth_in_usd = 1 / this.cost_usd_in_eth;
+            this.cost_cloud_in_eth = this.cost_cloud_in_usd  *  this.cost_usd_in_eth;
+           
         }
     });
     particlesJS.load('particles-js', 'src/particles/particles.json', function () {
